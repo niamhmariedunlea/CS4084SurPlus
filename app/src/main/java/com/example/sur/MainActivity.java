c9805200e9b1d3f696b12a4c483603025f43bce5
niamhmariedunlea <21145059@studentmail.ul.ie>
2023-03-05 13:19:15 +0000

    ND_add in onClickSignIn for the sign in button/image

--- app/src/main/java/com/example/sur/MainActivity.java
+++ app/src/main/java/com/example/sur/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.sur;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), RC_SIGN_IN); // generic code when they have signed in
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println("Sign in successful! \n" +
+                        "name = " + user.getDisplayName() + "\n" +
+                        "email = " + user.getEmail() + "\n" +
+                        "id = " + user.getUid());
+            } else { // if click the back button it will cancel
+                if (response == null) {
+                    System.out.println("Sign in cancelled");
+                    return;
+                } // or if the connection drops
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    System.out.println("No Internet Connection");
+                    return;
+                }
+            }
+        }
+    }
 }
+
