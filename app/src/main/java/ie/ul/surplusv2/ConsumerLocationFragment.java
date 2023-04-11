dce22cd7c6a990ef9ee7eecdc566d526ffeacdd0
Gabi <20226705@studentmail.ul.ie>
2023-04-11 23:01:41 +0100

    Build with profile layout, landing page layout semi done, bag/offers page started

--- app/src/main/java/ie/ul/surplusv2/ConsumerLocationFragment.java
+++ app/src/main/java/ie/ul/surplusv2/ConsumerLocationFragment.java
@@ -1,96 +1,75 @@
 package ie.ul.surplusv2;
 
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConsumerLocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ConsumerLocationFragment extends Fragment implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-    public ConsumerLocationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConsumerLocationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConsumerLocationFragment newInstance(String param1, String param2) {
-        ConsumerLocationFragment fragment = new ConsumerLocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
+    Button currentLoc;
+    SearchView searchview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_consumer_location, container, false);
-        mapFragment = (SupportMapFragment) getChildFragmentManager() .findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+
         if (mapFragment == null) {
             FragmentManager fm = getFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
-            mapFragment= SupportMapFragment.newInstance();
+            mapFragment = SupportMapFragment.newInstance();
             ft.replace(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
         return v;
-
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
     }
+
 }
