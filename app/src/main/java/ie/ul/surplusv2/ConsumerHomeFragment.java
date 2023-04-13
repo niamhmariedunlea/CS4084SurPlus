a352dc18df60483dc9b23ea5679fc328b70b5d0d
Gabi <20226705@studentmail.ul.ie>
2023-04-13 17:14:09 +0100

    Pulling user name for the greeting on the home pages (glitch occurs when signing in as a company so need to discuss plan for that

--- app/src/main/java/ie/ul/surplusv2/ConsumerHomeFragment.java
+++ app/src/main/java/ie/ul/surplusv2/ConsumerHomeFragment.java
@@ -3,14 +3,28 @@ package ie.ul.surplusv2;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class ConsumerHomeFragment extends Fragment {
+
+    TextView consumerName;
 
 
     @Override
@@ -20,4 +34,39 @@ public class ConsumerHomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_consumer_home, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        consumerName = getActivity().findViewById(R.id.textViewName);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentId = firebaseUser.getUid();
+        DocumentReference documentReference;
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+        documentReference = firebaseFirestore.collection("consumers").document(currentId);
+
+        documentReference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.getResult().exists())
+                        {
+                            String resultName = task.getResult().getString("fullName");
+
+                            consumerName.setText("Hi "+ resultName + "!");
+
+                        }else
+                        {
+                            Intent intent = new Intent(getActivity(), RegistrationActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+    }
 }
--- app/src/main/java/ie/ul/surplusv2/SupplierHomeFragment.java
+++ app/src/main/java/ie/ul/surplusv2/SupplierHomeFragment.java
@@ -1,64 +1,69 @@
 package ie.ul.surplusv2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SupplierHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class SupplierHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SupplierHomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SupplierHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SupplierHomeFragment newInstance(String param1, String param2) {
-        SupplierHomeFragment fragment = new SupplierHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView consumerName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_supplier_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        consumerName = getActivity().findViewById(R.id.textViewName);
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentId = firebaseUser.getUid();
+        DocumentReference documentReference;
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+        documentReference = firebaseFirestore.collection("suppliers").document(currentId);
+
+        documentReference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.getResult().exists())
+                        {
+                            String resultName = task.getResult().getString("fullName");
+
+                            consumerName.setText("Hi "+ resultName + "!");
+
+                        }else
+                        {
+                            Intent intent = new Intent(getActivity(), RegistrationActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
     }
 }
