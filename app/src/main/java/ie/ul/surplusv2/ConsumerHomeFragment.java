e47b4d59266625e758fff47bd35c49892f2c0dab
Gabi <20226705@studentmail.ul.ie>
2023-04-15 20:20:01 +0100

    Map Added to consumer homepage

--- app/src/main/java/ie/ul/surplusv2/ConsumerHomeFragment.java
+++ app/src/main/java/ie/ul/surplusv2/ConsumerHomeFragment.java
@@ -13,6 +13,15 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,28 +31,42 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-public class ConsumerHomeFragment extends Fragment {
+public class ConsumerHomeFragment extends Fragment implements OnMapReadyCallback {
 
     //textView on the home page where you'll use account name for personalization
     TextView consumerName;
+
+    MapView mMapView;
+
+    GoogleMap mGoogleMap;
+
+    Marker marker;
+
+    View hView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consumer_home, container, false);
+        hView = inflater.inflate(R.layout.fragment_consumer_home, container, false);
+        return hView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         consumerName = getActivity().findViewById(R.id.textViewName);
+
+        mMapView = (MapView) hView.findViewById(R.id.mapHome);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        mMapView.onStart();
         //Get currently sign-in user from firebase
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         String currentId = firebaseUser.getUid();
@@ -74,5 +97,51 @@ public class ConsumerHomeFragment extends Fragment {
 
                     }
                 });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap gMap) {
+        //When map is ready have zoom and map type already present
+
+        mGoogleMap = gMap;
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        LatLng latLng = new LatLng(52.6738, -8.5719);
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mGoogleMap.animateCamera(cameraUpdate);
+        marker = mGoogleMap.addMarker(markerOptions);
+    }
+
+
+    //All the following are required for MapView to work
+    @Override
+    public void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
