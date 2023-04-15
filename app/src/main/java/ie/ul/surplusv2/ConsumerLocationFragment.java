acfad5d0830726fb8333649b606e4c6607a29d26
Gabi <20226705@studentmail.ul.ie>
2023-04-15 19:07:09 +0100

    Maps fully operational

--- app/src/main/java/ie/ul/surplusv2/ConsumerLocationFragment.java
+++ app/src/main/java/ie/ul/surplusv2/ConsumerLocationFragment.java
@@ -6,6 +6,8 @@ import static androidx.constraintlayout.motion.widget.Debug.getLocation;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -14,9 +16,11 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,6 +43,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -47,81 +52,182 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 public class ConsumerLocationFragment extends Fragment implements OnMapReadyCallback {
 
-
     Button currentLocBtn;
-    EditText searchBar;
+    SearchView searchBar;
 
-    MapView mapView;
-    GoogleMap googleMap;
+    MapView mMapView;
+    GoogleMap mGoogleMap;
+    View mView;
+
+    Marker marker;
+
+    FusedLocationProviderClient client;
+
+    public ConsumerLocationFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //checkPerm();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_consumer_location, container, false);
+        mView = inflater.inflate(R.layout.fragment_consumer_location, container, false);
 
 
-        mapView = (MapView) v.findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-        
-        currentLocBtn = getActivity().findViewById(R.id.currentlocbtn);
-
-        return v;
-
+        return mView;
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        currentLocBtn = getActivity().findViewById(R.id.currentlocbtn);
+        searchBar = getActivity().findViewById(R.id.search_box);
+
+        currentLocBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Getting your Location...", Toast.LENGTH_SHORT).show();
+                getCurrentLocation();
+            }
+        });
+
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(getActivity(), "Searching...", Toast.LENGTH_SHORT).show();
+                searchLocationResult();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) { return false;}
+        });
+
+    }
+
+
+    @Override
     public void onMapReady(@NonNull GoogleMap gMap) {
-        LatLng latLng = new LatLng(53, 8);
-        googleMap = gMap;
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        googleMap.addMarker(new MarkerOptions().position(latLng));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53, -8), 7));
+
+        mGoogleMap = gMap;
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+       // mGoogleMap.setMyLocationEnabled(true);
+
+
+        /*
+
+        mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53, -8), 7));
+        LatLng latLng = new LatLng(53, -8);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53, -8), 7));
+         */
+
+    }
+
+    private void checkPerm() {
 
     }
 
     @Override
     public void onResume() {
-        mapView.onResume();
+        mMapView.onResume();
         super.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+        mMapView.onPause();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mapView.onStart();
+        mMapView.onStart();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mapView.onStop();
+        mMapView.onStop();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        mMapView.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        mMapView.onLowMemory();
     }
 
+    public void getCurrentLocation() {
+        client.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful())
+            {
+                Location location = task.getResult();
+                showCurrentLocation(location.getLatitude(), location.getLongitude());
 
+
+            }
+        });
+    }
+
+    private void showCurrentLocation(double latitude, double longitude) {
+        LatLng latLng = new LatLng(latitude, longitude);
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.animateCamera(cameraUpdate);
+        marker = mGoogleMap.addMarker(markerOptions);
+    }
+
+    private void searchLocationResult() {
+        String loc = searchBar.getQuery().toString();
+        if (loc == null) {
+            Toast.makeText(getActivity(), "Location Not Found", Toast.LENGTH_SHORT).show();
+        } else {
+            Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+            try {
+                List<Address> addressList = geocoder.getFromLocationName(loc, 1);
+                if (addressList.size() > 0) {
+                    LatLng latlng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
+                    if (marker != null) {
+                        marker.remove();
+                    }
+                    MarkerOptions markerOptions = new MarkerOptions().position(latlng).title(loc);
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+                    mGoogleMap.animateCamera(cameraUpdate);
+                    marker = mGoogleMap.addMarker(markerOptions);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 }
